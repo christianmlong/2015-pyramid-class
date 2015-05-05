590110c78a6521f7751ef69b415c7e83f20eedd8
Christian Long <christianzlong@gmail.com>
2015-05-05 11:58:01 -0500

    Add a functional test
    To get this test to run, install the WebTest package
    
        pip install WebTest

--- hello.py
+++ hello.py
@@ -2,16 +2,17 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 
-
 def hello_world(request):
     return Response('Hello world!')
 
-if __name__ == '__main__':
+def main():
     config = Configurator()
     config.add_route('hello', '/hello')
     config.add_view(hello_world, route_name='hello')
     app = config.make_wsgi_app()
+    return app
+
+if __name__ == '__main__':
+    app = main()
     server = make_server('0.0.0.0', 8080, app)
     server.serve_forever()
-   
-
--- test_hello.py
+++ test_hello.py
@@ -6,5 +6,13 @@ class HelloWorldUnitTests(unittest.TestCase):
         result = hello_world({})
         self.assertEqual(result.body, b'Hello world!')
 
-if __name__ == '__main__':
-    unittest.main()
+class HelloWorldFunctionalTests(unittest.TestCase):
+    def setUp(self):
+        from hello import main
+        app = main()
+        from webtest import TestApp
+        self.testapp = TestApp(app)
+
+    def test_without_slash(self):
+        res = self.testapp.get('/hello', status=200)
+        self.assertTrue(b'Hello world!' in res.body)
